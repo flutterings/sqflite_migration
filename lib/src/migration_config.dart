36a4011fdd0def1469c28bb154f3edb0d0b93319
Marco Ivarsson <marco.ivarsson@gmail.com>
2021-03-09 11:13:54 +0100

    Support null-safety

--- lib/src/migration_config.dart
+++ lib/src/migration_config.dart
@@ -13,9 +13,5 @@ class MigrationConfig {
   final List<String> initializationScript;
   final List<String> migrationScripts;
 
-  MigrationConfig({this.initializationScript, this.migrationScripts})
-      : assert(initializationScript != null,
-            'The initializationScript cannot be null.'),
-        assert(
-            migrationScripts != null, 'The migrationScripts cannot be null.');
+  MigrationConfig({required this.initializationScript, required this.migrationScripts});
 }
--- test/src/open_database_with_migration_test.dart
+++ test/src/open_database_with_migration_test.dart
@@ -1,23 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'package:sqflite_migration/sqflite_migration.dart';
 
-class MockConfig extends Mock implements MigrationConfig {}
-
 void main() {
   test('should pass the path to sqflite', () async {
-    final config = MockConfig();
-    when(config.migrationScripts).thenReturn([]);
+    final config = MigrationConfig(
+      initializationScript: [],
+      migrationScripts: [],
+    );
+
     final log = [];
 
     await openDatabaseWithMigration('path', config,
         openDatabase: (
           path, {
-          int version,
-          OnDatabaseCreateFn onCreate,
-          OnDatabaseVersionChangeFn onUpgrade,
+          int? version,
+          OnDatabaseCreateFn? onCreate,
+          OnDatabaseVersionChangeFn? onUpgrade,
         }) =>
             log.add(path));
 
@@ -27,16 +27,19 @@ void main() {
   test(
       'should determine the version according to the number of migration scripts',
       () async {
-    final config = MockConfig();
-    when(config.migrationScripts).thenReturn(['script 1', 'script 2']);
+    final config = MigrationConfig(
+      initializationScript: [],
+      migrationScripts: ['script 1', 'script 2'],
+    );
+
     final log = [];
 
     await openDatabaseWithMigration('path', config,
         openDatabase: (
           path, {
-          int version,
-          OnDatabaseCreateFn onCreate,
-          OnDatabaseVersionChangeFn onUpgrade,
+          int? version,
+          OnDatabaseCreateFn? onCreate,
+          OnDatabaseVersionChangeFn? onUpgrade,
         }) =>
             log.add(version));
 
